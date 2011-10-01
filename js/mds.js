faed29d255014484b6efa0117664b28b840d0e7c
reddogcct <reddoggato@gmail.com>
2011-10-01 17:07:05 -0400

    Alpha update with code foundation

---
+++ js/mds.js
@@ -0,0 +1,17 @@
+// mds JavaScript Library
+// Author: Marcos DeSousa
+// Version 1.0 alpha
+
+var mds = function(){
+	return new mds.prototype.init();
+};//close mds function
+
+mds.prototype = {
+
+	init: function(){
+		//init the library
+	}//close init function
+	
+};//close mds prototype
+
+mds.prototype.init.prototype = mds.prototype;
